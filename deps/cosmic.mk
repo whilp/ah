a229baa124c49cc549f58983f2344101e2e40546
Will Maier <189851+whilp@users.noreply.github.com>
2026-03-01 17:04:30 -0800

    Bump cosmic to 2026-03-02-c363a79 (#464)

--- deps/cosmic.mk
+++ deps/cosmic.mk
@@ -1,4 +1,4 @@
-cosmic_url := https://github.com/whilp/cosmic/releases/download/2026-02-28-4cc5b3b/cosmic-lua
-cosmic_sha := cb7db25b5199bfbd680425a522687bc61dcbedbb5b22d1b31c1f6a9def6e060a
-cosmic_debug_url := https://github.com/whilp/cosmic/releases/download/2026-02-28-4cc5b3b/cosmic-lua-debug
-cosmic_debug_sha := 6502d390cbd55b6eefeea98d96e0a55cd87d831dd8bdb546571da80b443e9199
+cosmic_url := https://github.com/whilp/cosmic/releases/download/2026-03-02-c363a79/cosmic-lua
+cosmic_sha := 9dc7fc906431ea5f40d18a0710010aa231a762edcd13e20c718cda91c6be91f9
+cosmic_debug_url := https://github.com/whilp/cosmic/releases/download/2026-03-02-c363a79/cosmic-lua-debug
+cosmic_debug_sha := 74f4722155852f7e8a560d5759bbd1879226dcba52ef1a9015219204743b29ad
